14346da3dc155dc9fc42298d39cb5eef7690ebc6
Des <45636848+teachwaititi@users.noreply.github.com>
2022-06-08 10:05:08 -0300

    Create guesses2.js

---
+++ res/guesses2.js
@@ -0,0 +1,28 @@
+const GUESSES = [
+  'BORIS',
+  'CRANE',
+  'DAVID',
+  'DARBY',
+  'ELIZA',
+  'FAXON',
+  'FRIAS',
+  'HANDS',
+  'JONES',
+  'LOUIS',
+  'NACHO',
+  'NAIRN',
+  'NIGEL',
+  'ORTIZ',
+  'ROACH',
+  'SAMBA',
+  'STEDE',
+  'STEIN',
+  'SWEDE',
+  'TAIKA',
+  'ZAYRE',
+];
+
+const INFO_WON = [
+  'Information about <br>flags',
+  'Information about <br>mates',
+];
