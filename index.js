da6706e1f1e30a1a0a1ffe69cd202d1a5e1f9c41
teachwaititi <pereirandressa92@gmail.com>
2022-06-08 21:19:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,17 +25,7 @@ let currentWord = '';
 // Get everything setup and the game responding to user actions.
 const init = () => {
 
-  // var currentTime = new Date().getTime();
-  // if (currentTime - lastUpdateTime >= (24*60*60*1000)) // number of milliseconds in a day
-  // {
-  //   if (index_day > GUESSES.length) {
-  //     index_day = 0;
-  //   } else {
-  //     index_day += 1;
-  //   }
-  //   WORD_OF_THE_DAY = GUESSES[index_day];
-  //   lastUpdateTime = currentTime;
-  // }
+  runOncePerDay();
 
   console.log('👋 Welcome to Crewdle');
 
@@ -333,3 +323,26 @@ $(document).on('click', '.popup_info', function(){
 $(document).on('click', '.hover_info', function(){
   $('.hover_info').hide();
 });
+
+function hasOneDayPassed()
+  // get today's date. eg: "7/37/2007"
+  var today = new Date().toLocaleDateString();
+
+  // if there's a date in localstorage and it's equal to the above:
+  // inferring a day has yet to pass since both dates are equal.
+  if ( localStorage.yourapp_date == today )
+      return false;
+
+  // this portion of logic occurs when a day has passed
+  localStorage.yourapp_date = today;
+  return true;
+}
+
+
+// some function which should run once a day
+function runOncePerDay() {
+  if( !hasOneDayPassed() ) return false;
+
+  // your code below
+  alert('Good morning!');
+}
